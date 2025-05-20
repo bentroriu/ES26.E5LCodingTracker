10cc6b2b15e405440e725a7b886569531ee599ad
Joshua Caviente <jpcaviente@up.edu.ph>
2025-05-20 22:33:36 +0800

    Console.Writeline Modified

--- Coding Tracker/Form1.Designer.vb
+++ Coding Tracker/Form1.Designer.vb
@@ -22,10 +22,176 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.pnlMapGrid = New System.Windows.Forms.Panel()
+        Me.grpTools = New System.Windows.Forms.GroupBox()
+        Me.btnPowerLine = New System.Windows.Forms.Button()
+        Me.btnBuilding = New System.Windows.Forms.Button()
+        Me.btnBridge = New System.Windows.Forms.Button()
+        Me.btnRoad = New System.Windows.Forms.Button()
+        Me.grpProperties = New System.Windows.Forms.GroupBox()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnSimulate = New System.Windows.Forms.Button()
+        Me.cmbMode = New System.Windows.Forms.ComboBox()
+        Me.lblMode = New System.Windows.Forms.Label()
+        Me.lblPower = New System.Windows.Forms.Label()
+        Me.lblBudget = New System.Windows.Forms.Label()
+        Me.grpTools.SuspendLayout()
+        Me.grpProperties.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'pnlMapGrid
+        '
+        Me.pnlMapGrid.BackColor = System.Drawing.Color.DimGray
+        Me.pnlMapGrid.Location = New System.Drawing.Point(189, 0)
+        Me.pnlMapGrid.Name = "pnlMapGrid"
+        Me.pnlMapGrid.Size = New System.Drawing.Size(744, 537)
+        Me.pnlMapGrid.TabIndex = 0
+        '
+        'grpTools
+        '
+        Me.grpTools.Controls.Add(Me.btnPowerLine)
+        Me.grpTools.Controls.Add(Me.btnBuilding)
+        Me.grpTools.Controls.Add(Me.btnBridge)
+        Me.grpTools.Controls.Add(Me.btnRoad)
+        Me.grpTools.Location = New System.Drawing.Point(2, 147)
+        Me.grpTools.Name = "grpTools"
+        Me.grpTools.Size = New System.Drawing.Size(181, 253)
+        Me.grpTools.TabIndex = 1
+        Me.grpTools.TabStop = False
+        Me.grpTools.Text = "Tools"
+        '
+        'btnPowerLine
+        '
+        Me.btnPowerLine.Location = New System.Drawing.Point(27, 170)
+        Me.btnPowerLine.Name = "btnPowerLine"
+        Me.btnPowerLine.Size = New System.Drawing.Size(127, 36)
+        Me.btnPowerLine.TabIndex = 6
+        Me.btnPowerLine.Text = "Powerline"
+        Me.btnPowerLine.UseVisualStyleBackColor = True
+        '
+        'btnBuilding
+        '
+        Me.btnBuilding.Location = New System.Drawing.Point(27, 128)
+        Me.btnBuilding.Name = "btnBuilding"
+        Me.btnBuilding.Size = New System.Drawing.Size(127, 36)
+        Me.btnBuilding.TabIndex = 5
+        Me.btnBuilding.Text = "Building"
+        Me.btnBuilding.UseVisualStyleBackColor = True
+        '
+        'btnBridge
+        '
+        Me.btnBridge.Location = New System.Drawing.Point(27, 86)
+        Me.btnBridge.Name = "btnBridge"
+        Me.btnBridge.Size = New System.Drawing.Size(127, 36)
+        Me.btnBridge.TabIndex = 4
+        Me.btnBridge.Text = "Bridge"
+        Me.btnBridge.UseVisualStyleBackColor = True
+        '
+        'btnRoad
+        '
+        Me.btnRoad.Location = New System.Drawing.Point(27, 44)
+        Me.btnRoad.Name = "btnRoad"
+        Me.btnRoad.Size = New System.Drawing.Size(127, 36)
+        Me.btnRoad.TabIndex = 3
+        Me.btnRoad.Text = "Road"
+        Me.btnRoad.UseVisualStyleBackColor = True
+        '
+        'grpProperties
+        '
+        Me.grpProperties.Controls.Add(Me.btnReset)
+        Me.grpProperties.Controls.Add(Me.btnSimulate)
+        Me.grpProperties.Controls.Add(Me.cmbMode)
+        Me.grpProperties.Controls.Add(Me.lblMode)
+        Me.grpProperties.Controls.Add(Me.lblPower)
+        Me.grpProperties.Controls.Add(Me.lblBudget)
+        Me.grpProperties.Location = New System.Drawing.Point(939, 147)
+        Me.grpProperties.Name = "grpProperties"
+        Me.grpProperties.Size = New System.Drawing.Size(181, 253)
+        Me.grpProperties.TabIndex = 2
+        Me.grpProperties.TabStop = False
+        Me.grpProperties.Text = "Properties"
+        '
+        'btnReset
+        '
+        Me.btnReset.Location = New System.Drawing.Point(28, 170)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(127, 36)
+        Me.btnReset.TabIndex = 5
+        Me.btnReset.Text = "Reset"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'btnSimulate
+        '
+        Me.btnSimulate.Location = New System.Drawing.Point(28, 128)
+        Me.btnSimulate.Name = "btnSimulate"
+        Me.btnSimulate.Size = New System.Drawing.Size(127, 36)
+        Me.btnSimulate.TabIndex = 4
+        Me.btnSimulate.Text = "Simulate"
+        Me.btnSimulate.UseVisualStyleBackColor = True
+        '
+        'cmbMode
+        '
+        Me.cmbMode.FormattingEnabled = True
+        Me.cmbMode.Location = New System.Drawing.Point(66, 93)
+        Me.cmbMode.Name = "cmbMode"
+        Me.cmbMode.Size = New System.Drawing.Size(109, 24)
+        Me.cmbMode.TabIndex = 3
+        '
+        'lblMode
+        '
+        Me.lblMode.AutoSize = True
+        Me.lblMode.Location = New System.Drawing.Point(15, 96)
+        Me.lblMode.Name = "lblMode"
+        Me.lblMode.Size = New System.Drawing.Size(45, 16)
+        Me.lblMode.TabIndex = 2
+        Me.lblMode.Text = "Mode:"
+        '
+        'lblPower
+        '
+        Me.lblPower.AutoSize = True
+        Me.lblPower.Location = New System.Drawing.Point(15, 64)
+        Me.lblPower.Name = "lblPower"
+        Me.lblPower.Size = New System.Drawing.Size(82, 16)
+        Me.lblPower.TabIndex = 1
+        Me.lblPower.Text = "Total Power:"
+        '
+        'lblBudget
+        '
+        Me.lblBudget.AutoSize = True
+        Me.lblBudget.Location = New System.Drawing.Point(15, 31)
+        Me.lblBudget.Name = "lblBudget"
+        Me.lblBudget.Size = New System.Drawing.Size(53, 16)
+        Me.lblBudget.TabIndex = 0
+        Me.lblBudget.Text = "Budget:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1125, 538)
+        Me.Controls.Add(Me.grpProperties)
+        Me.Controls.Add(Me.grpTools)
+        Me.Controls.Add(Me.pnlMapGrid)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.grpTools.ResumeLayout(False)
+        Me.grpProperties.ResumeLayout(False)
+        Me.grpProperties.PerformLayout()
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents pnlMapGrid As Panel
+    Friend WithEvents grpTools As GroupBox
+    Friend WithEvents btnPowerLine As Button
+    Friend WithEvents btnBuilding As Button
+    Friend WithEvents btnBridge As Button
+    Friend WithEvents btnRoad As Button
+    Friend WithEvents grpProperties As GroupBox
+    Friend WithEvents cmbMode As ComboBox
+    Friend WithEvents lblMode As Label
+    Friend WithEvents lblPower As Label
+    Friend WithEvents lblBudget As Label
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnSimulate As Button
 End Class
--- Coding Tracker/Form1.vb
+++ Coding Tracker/Form1.vb
@@ -1,3 +1,84 @@
-﻿Public Class Form1
+﻿
+Public Class Form1
+        Dim budget As Double = 100000
+        Dim totalPower As Double = 0
+        Dim selectedTool As String = ""
+        Dim isSandboxMode As Boolean = False
 
-End Class
+        Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+            cmbMode.Items.Add("Constrained Mode")
+            cmbMode.Items.Add("Sandbox Mode")
+            cmbMode.SelectedIndex = 0
+
+            lblBudget.Text = "Budget: ₱" & budget.ToString("N2")
+            lblPower.Text = "Total Power: 0 W"
+        End Sub
+
+        Private Sub btnRoad_Click(sender As Object, e As EventArgs) Handles btnRoad.Click
+            selectedTool = "Road"
+        End Sub
+
+        Private Sub btnBridge_Click(sender As Object, e As EventArgs) Handles btnBridge.Click
+            selectedTool = "Bridge"
+        End Sub
+
+        Private Sub btnBuilding_Click(sender As Object, e As EventArgs) Handles btnBuilding.Click
+            selectedTool = "Building"
+        End Sub
+
+        Private Sub btnPowerLine_Click(sender As Object, e As EventArgs) Handles btnPowerLine.Click
+            selectedTool = "PowerLine"
+        End Sub
+
+        Private Sub cmbMode_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbMode.SelectedIndexChanged
+            isSandboxMode = cmbMode.SelectedItem.ToString() = "Sandbox Mode"
+        End Sub
+
+        Private Sub pnlMapGrid_MouseClick(sender As Object, e As MouseEventArgs) Handles pnlMapGrid.MouseClick
+            Dim cost As Double = 0
+            Dim powerUse As Double = 0
+
+            Select Case selectedTool
+                Case "Road"
+                    cost = 5000
+                Case "Bridge"
+                    cost = 15000
+                Case "Building"
+                    cost = 20000
+                    powerUse = 300 ' watts
+                Case "PowerLine"
+                    cost = 10000
+            End Select
+
+            If Not isSandboxMode AndAlso budget < cost Then
+                MessageBox.Show("Not enough budget!")
+                Return
+            End If
+
+            If Not isSandboxMode Then
+                budget -= cost
+                lblBudget.Text = "Budget: ₱" & budget.ToString("N2")
+            End If
+
+            totalPower += powerUse
+            lblPower.Text = "Total Power: " & totalPower.ToString("F0") & " W"
+
+            ' Simple visualization: draw rectangle
+            Dim g As Graphics = pnlMapGrid.CreateGraphics()
+            g.FillRectangle(Brushes.LightGray, e.X, e.Y, 30, 30)
+            g.DrawString(selectedTool.Substring(0, 1), Me.Font, Brushes.Black, e.X + 5, e.Y + 5)
+        End Sub
+
+        Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+            budget = 100000
+            totalPower = 0
+            lblBudget.Text = "Budget: ₱" & budget.ToString("N2")
+            lblPower.Text = "Total Power: 0 W"
+            pnlMapGrid.Invalidate() ' Clears drawing
+        End Sub
+
+        Private Sub btnSimulate_Click(sender As Object, e As EventArgs) Handles btnSimulate.Click
+            MessageBox.Show("Simulation complete!" & vbCrLf & "Total Power: " & totalPower & " W")
+        End Sub
+    End Class
+
